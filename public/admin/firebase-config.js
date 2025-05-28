d7a2ba31a5e5849891f5a7c8f21d5482b2bf403e
Magna_j <you@example.com>
2025-05-28 16:42:18 +0300

    Removed hardcoded Firebase config and switched to env variables

--- public/admin/firebase-config.js
+++ public/admin/firebase-config.js
@@ -1,21 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDuAbMU5SYXJ3i9hw86rqUft2SeZJQOlfY",
-  authDomain: "cinenovatv-66478.firebaseapp.com",
-  projectId: "cinenovatv-66478",
-  storageBucket: "cinenovatv-66478.firebasestorage.app",
-  messagingSenderId: "209338168342",
-  appId: "1:209338168342:web:6c38766dc658ecfd817137",
-  measurementId: "G-NXB2GVL4X8"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
