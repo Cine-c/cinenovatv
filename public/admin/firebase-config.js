ede6c2f82ef5f4377d239532ccd5b012987badeb
Magna_j <you@example.com>
2025-05-28 16:17:32 +0300

    Remove leaked API key and update config

--- public/admin/firebase-config.js
+++ public/admin/firebase-config.js
@@ -1,22 +1,21 @@
-// public/admin/firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-storage.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB1OJrnV1feRYUcUvzOLCiGaN_1w3dEut8",
-  authDomain: "cinenovatv.firebaseapp.com",
-  projectId: "cinenovatv",
-  storageBucket: "cinenovatv.firebasestorage.app",
-  messagingSenderId: "151330277063",
-  appId: "1:151330277063:web:f3766dc9c2ac5f68fc20e2",
-  measurementId: "G-9GTVV8JYFQ"
+  apiKey: "AIzaSyDuAbMU5SYXJ3i9hw86rqUft2SeZJQOlfY",
+  authDomain: "cinenovatv-66478.firebaseapp.com",
+  projectId: "cinenovatv-66478",
+  storageBucket: "cinenovatv-66478.firebasestorage.app",
+  messagingSenderId: "209338168342",
+  appId: "1:209338168342:web:6c38766dc658ecfd817137",
+  measurementId: "G-NXB2GVL4X8"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-const storage = getStorage(app);
-
-export { auth, db, storage };
+const analytics = getAnalytics(app);
